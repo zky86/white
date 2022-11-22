6c5e2c7fd840d93174b86d60b8b34d0776d5dd02
zhengkangyuan <272495222@qq.com>
2022-11-22 17:51:36 +0800

    数据库链接

--- wj/src/main/java/com/evan/wj/pojo/User.java
+++ wj/src/main/java/com/evan/wj/pojo/User.java
@@ -1,7 +1,19 @@
 package com.evan.wj.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@JsonIgnoreProperties({"handler","hibernateLazyInitializer"})
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     int id;
+
     String username;
     String password;
 
